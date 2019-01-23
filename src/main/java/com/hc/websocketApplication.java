eea82f482144a8bf265f2435765cf8c9fbcd008e
xiaoli <1651258595@qq.com>
2019-01-23 17:56:31 +0800

    初步完成

--- src/main/java/com/hc/websocketApplication.java
+++ src/main/java/com/hc/websocketApplication.java
@@ -10,10 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class websocketApplication {
 	
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(websocketApplication.class, args);
